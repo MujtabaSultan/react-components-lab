9484fc517a1434f764dba8ecafbba82b2ae84a60
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-08-05 14:49:35 +0300

    done

---
+++ src/components/weather/weather.jsx
@@ -0,0 +1,37 @@
+// src/App.jsx
+
+
+
+
+const Weather = ({forcast}) => {
+
+  const {day,img,imgAlt,conditions,time} = forcast
+
+  return (
+    <>
+    
+<div className="weather">
+  <h2>{day}</h2>
+  <img src={img} alt={imgAlt} />
+  <p><span>conditions: </span>{conditions}</p>
+  <p><span>time: </span>{time}</p>
+</div>
+    
+    </>
+  );
+}
+
+export default Weather
+
+
+
+
+
+
+
+
+
+
+
+
+
